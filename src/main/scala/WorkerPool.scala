edc3c7331a47884ee0acc9e3bddc1d42f21d9b17
Romain <filirom1@gmail.com>
2010-10-28 00:30:14 +0200

    Add SessionManagement

--- src/main/scala/WorkerPool.scala
+++ src/main/scala/WorkerPool.scala
@@ -1,5 +1,6 @@
 package org.scalabilityissues
 
+import java.util.UUID
 import se.scalablesolutions.akka.actor.{ActorRef, Actor}
 import se.scalablesolutions.akka.util.Logging
 import collection.mutable.HashMap
@@ -22,33 +23,96 @@ import collection.mutable.HashMap
  */
 
 sealed class Message
-case class Register(actor : ActorRef) extends Message
-case class UnRegister (actor: ActorRef) extends Message
+case class Register(actor: ActorRef) extends Message
+case class UnRegister(actor: ActorRef) extends Message
 case class NewSession() extends Message
-case class ReleaseSession(id : String) extends Message
+case class ReleaseSession(id: String) extends Message
 case class Execute(id: String, cmd: Object)
 case object HowManyWorkersRegistered
 case object HowManyWorkersFree
-case object HowManyWorkersBusy
+case object HowManyWorkersReserved
+case object NoFreeWorkerAvailable
 
+case class WorkerDescription(worker: ActorRef, var sessionId : Option[String])
 
-trait WorkerPool { this: Actor =>
+trait WorkerPool {
+  this: Actor =>
   import self._
-  val workers = new HashMap[String,ActorRef]
 
-  protected def registrationManagement : Receive = {
+  /**
+   * HashMap[actorId -> ActorRef]
+   */
+  val workers = new HashMap[String, WorkerDescription]
+
+  /**
+   *  Register new workers,
+   * and unregister known workers.
+   *
+   * New workers are set as free.
+   * Busy as free workers can be unregistered.
+   */
+  protected def registrationManagement: Receive = {
     case msg@Register(actor) => {
-      log.debug("WorkerPool receive Register with "+actor)
-      workers += (actor.id -> actor)
+      log.debug("WorkerPool receive Register with " + actor)
+      workers += (actor.id -> new WorkerDescription(actor,None))
     }
     case msg@UnRegister(actor) => {
-      log.debug("WorkerPool receive UnRegister with "+actor)
+      log.debug("WorkerPool receive UnRegister with " + actor)
       workers -= actor.id
     }
     case HowManyWorkersRegistered => {
       log.debug("WorkerPool receive HowManyWorkersRegistered")
       reply(workers.size)
     }
-  
+  }
+
+
+  /**
+   * Allocate a free worker to a new session, the worker become reserved.
+   *
+   * Or release a session in order to free a reserved worker.
+   *
+   * The sessionId is used to execute commands on a reserved worker.
+   */
+  protected def sessionManagement: Receive = {
+    case msg@NewSession() => {
+      log.debug("WorkerPool receive NewSession")
+      workers.values.find { worker => !worker.sessionId.isDefined } match {
+        case None => {
+          log.info("No free worker avalaible.")
+          reply(NoFreeWorkerAvailable)
+        }
+        case Some(freeWorker : WorkerDescription) => {
+          val id = UUID.randomUUID.toString
+          freeWorker.sessionId = Some(id)
+          reply(id)
+        }
+      }
+
+    }
+    case msg@ReleaseSession(id) => {
+      log.debug("WorkerPool receive ReleaseSession")
+      workers.values.find { worker => worker.sessionId == Some(id) } match {
+        case None => {
+          log.warning("Worker can not be release, because not found, with id : " + id)
+
+        }
+        case Some(freeWorker : WorkerDescription) => {
+           freeWorker.sessionId = None
+        }
+      }
+
+    }
+    case msg@Execute(id, cmd) => {
+      log.debug("WorkerPool receive Execute")
+    }
+    case HowManyWorkersFree => {
+      log.debug("WorkerPool receive HowManyWorkersFree")
+      reply (workers.values.filter { worker => !worker.sessionId.isDefined }.size)
+    }
+    case HowManyWorkersReserved => {
+      log.debug("WorkerPool receive HowManyWorkersReserved")
+      reply (workers.values.filter { worker => worker.sessionId.isDefined }.size)
+    }
   }
 }
--- src/test/scala/WorkerPoolSpec.scala
+++ src/test/scala/WorkerPoolSpec.scala
@@ -1,4 +1,5 @@
 package org.scalabilityissues
+
 import se.scalablesolutions.akka.actor.Actor
 import se.scalablesolutions.akka.util.Logging
 import org.scalatest.{GivenWhenThen, FeatureSpec}
@@ -22,8 +23,8 @@ import Actor._
  * the License.
  */
 class WorkerPoolSpec extends FeatureSpec with GivenWhenThen with ShouldMatchers {
-  feature("Workers can be registered and unregistered in the WorkerPool"){
-    scenario("A Worker is registered, in an empty WorkerPool"){
+  feature("Workers can be registered and unregistered in the WorkerPool") {
+    scenario("A Worker is registered, in an empty WorkerPool") {
       given("An Empty WorkerPool")
       val pool = actorOf[WorkerPoolImpl].start
 
@@ -34,10 +35,10 @@ class WorkerPoolSpec extends FeatureSpec with GivenWhenThen with ShouldMatchers 
       pool !! HowManyWorkersRegistered should be(Some(1))
     }
 
-    scenario("A Worker is registered, in the WorkerPool"){
+    scenario("A Worker is registered, in the WorkerPool") {
       given("An WorkerPool with registered Workers")
       val pool = actorOf[WorkerPoolImpl].start
-      for (i <- 1 to 3) pool ! new Register(actorOf(new Worker("worker "+i)).start)
+      for (i <- 1 to 3) pool ! new Register(actorOf(new Worker("worker " + i)).start)
       when("A Worker is registered")
       pool ! new Register(actorOf(new Worker("worker 4")).start)
 
@@ -45,10 +46,10 @@ class WorkerPoolSpec extends FeatureSpec with GivenWhenThen with ShouldMatchers 
       pool !! HowManyWorkersRegistered should be(Some(4))
     }
 
-    scenario("A Worker is unregistered, in the WorkerPool"){
+    scenario("A Worker is unregistered, in the WorkerPool") {
       given("A WorkerPool with registered Workers")
       val pool = actorOf[WorkerPoolImpl].start
-      val worker1  =actorOf(new Worker("worker 1")).start;
+      val worker1 = actorOf(new Worker("worker 1")).start;
       pool ! new Register(worker1)
       pool ! new Register(actorOf(new Worker("worker 2")).start)
       pool ! new Register(actorOf(new Worker("worker 3")).start)
@@ -60,15 +61,69 @@ class WorkerPoolSpec extends FeatureSpec with GivenWhenThen with ShouldMatchers 
       pool !! HowManyWorkersRegistered should be(Some(2))
     }
   }
+
+
+  feature("A client can use a Worker within a session.") {
+    scenario("A Worker is registered, and the number of free Workers is 1") {
+      given("A WorkerPool with one Worker")
+      val pool = actorOf[WorkerPoolImpl].start
+      pool ! new Register(actorOf(new Worker("worker 1")).start)
+
+
+      then("The Number of Free Workers is 1")
+      pool !! HowManyWorkersFree should be(Some(1))
+      pool !! HowManyWorkersReserved should be(Some(0))
+    }
+
+    scenario("A Worker is registered, and a client ask for a Worker") {
+      given("A WorkerPool with one Worker")
+      val pool = actorOf[WorkerPoolImpl].start
+      pool ! new Register(actorOf(new Worker("worker 1")).start)
+
+      when("A client ask for a worker")
+      (pool !! new NewSession())should be('defined)
+
+      then("The Number of Reserved Workers is 1")
+      pool !! HowManyWorkersFree should be(Some(0))
+      pool !! HowManyWorkersReserved should be(Some(1))
+    }
+
+    scenario("3 Workers are registered, and a client ask for a Worker") {
+      given("A WorkerPool with 3 Workers")
+      val pool = actorOf[WorkerPoolImpl].start
+      for (i <- 1 to 3) pool ! new Register(actorOf(new Worker("worker " + i)).start)
+
+      when("A client ask for a worker")
+      (pool !! new NewSession()) should be('defined)
+
+      then("The Number of Free Workers is 2")
+      pool !! HowManyWorkersFree should be(Some(2))
+      pool !! HowManyWorkersReserved should be(Some(1))
+    }
+
+    scenario("A Client reserved a Worker, and release the session.") {
+      given("A WorkerPool with one reserved Worker")
+      val pool = actorOf[WorkerPoolImpl].start
+      pool ! new Register(actorOf(new Worker("worker 1")).start)
+      val sessionId = (pool !! new NewSession()).as[String].getOrElse(throw new Exception("No session Id received"))
+
+      when("A client release the session")
+      pool ! new ReleaseSession(sessionId)
+  
+      then("The Number of Reserved Workers is 0")
+      pool !! HowManyWorkersFree should be(Some(1))
+      pool !! HowManyWorkersReserved should be(Some(0))
+    }
+  }
 }
 
-class WorkerPoolImpl extends Actor with WorkerPool{
-  def receive = registrationManagement
+class WorkerPoolImpl extends Actor with WorkerPool {
+  def receive = registrationManagement orElse sessionManagement
 }
 
-class Worker(id : String) extends Actor{
+class Worker(id: String) extends Actor {
   import self._
-  self.id=id
+  self.id = id
   def receive = {
     case _ => log.info("Receive a message")
   }
